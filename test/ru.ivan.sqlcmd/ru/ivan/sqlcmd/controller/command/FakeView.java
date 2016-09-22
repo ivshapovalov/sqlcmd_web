cddd937f4aebb59d6a56a21e90641ae465e86b2e
Ivan <metaldaze@mail.ru>
2016-09-22 17:49:07 +0300

    some refactoring

--- test/ru.ivan.sqlcmd/ru/ivan/sqlcmd/controller/command/FakeView.java
+++ test/ru.ivan.sqlcmd/ru/ivan/sqlcmd/controller/command/FakeView.java
@@ -8,16 +8,25 @@ import ru.ivan.sqlcmd.view.View;
 public class FakeView implements View {
 
     private String messages="";
+    private String input=null;
 
     @Override
     public String read() {
-        return null;
+        if (this.input==null) {
+            throw new IllegalStateException("Для работы проинициализируйте метод read");
+        }
+        String result=this.input;
+        this.input=null;
+        return result;
     }
 
     @Override
     public void write(String message) {
         messages+=message+"\n";
 
+    }
+    public void addRead(String input){
+        this.input=input;
     }
 
     public String getContent(){
