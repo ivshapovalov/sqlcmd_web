494228268b487075be0917a77c1825c5a8ddb7d9
Ivan <ivshapovalov@gmail.com>
2016-11-03 16:51:48 +0300

    serlets1

--- src/main/java/ru/ivan/sqlcmd/controller/web/MainServlet.java
+++ src/main/java/ru/ivan/sqlcmd/controller/web/MainServlet.java
@@ -2,15 +2,26 @@ package ru.ivan.sqlcmd.controller.web;
 
 
 import ru.ivan.sqlcmd.model.DatabaseManager;
-import java.io.IOException;
+import ru.ivan.sqlcmd.service.Service;
+import ru.ivan.sqlcmd.service.ServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 public class MainServlet extends HttpServlet {
+
+    private Service service;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+
+        service = new ServiceImpl();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -20,7 +31,7 @@ public class MainServlet extends HttpServlet {
 
         if (action.startsWith("/connect")) {
             if (manager == null) {
-                req.getRequestDispatcher("qwe.html").include(req, resp);
+                //req.getRequestDispatcher("qwe.html").include(req, resp);
                 req.getRequestDispatcher("connect.jsp").forward(req, resp);
             } else {
                 resp.sendRedirect(resp.encodeRedirectURL("menu"));
@@ -28,26 +39,26 @@ public class MainServlet extends HttpServlet {
             return;
         }
 
-//        if (manager == null) {
-//            resp.sendRedirect(resp.encodeRedirectURL("connect"));
-//            return;
-//        }
+        if (manager == null) {
+            resp.sendRedirect(resp.encodeRedirectURL("connect"));
+            return;
+        }
 
         if (action.startsWith("/menu") || action.equals("/")) {
-           // req.setAttribute("items", service.commandsList());
+            req.setAttribute("items", service.commandsList());
             req.getRequestDispatcher("menu.jsp").forward(req, resp);
 
         } else if (action.startsWith("/help")) {
             req.getRequestDispatcher("help.jsp").forward(req, resp);
+
+        } else if (action.startsWith("/rows")) {
+            String tableName = req.getParameter("table");
+            req.setAttribute("table", service.rows(manager, tableName));
+            req.getRequestDispatcher("rows.jsp").forward(req, resp);
+
+        } else {
+            req.getRequestDispatcher("error.jsp").forward(req, resp);
         }
-//        } else if (action.startsWith("/find")) {
-//            String tableName = req.getParameter("table");
-//            //req.setAttribute("table", service.find(manager, tableName));
-//            req.getRequestDispatcher("find.jsp").forward(req, resp);
-//
-//        } else {
-//            req.getRequestDispatcher("error.jsp").forward(req, resp);
-//        }
     }
 
     private String getAction(HttpServletRequest req) {
@@ -55,24 +66,24 @@ public class MainServlet extends HttpServlet {
         return requestURI.substring(req.getContextPath().length(), requestURI.length());
     }
 
-//    @Override
-//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        String action = getAction(req);
-//
-//        if (action.startsWith("/connect")) {
-//            String databaseName = req.getParameter("dbname");
-//            String userName = req.getParameter("username");
-//            String password = req.getParameter("password");
-//
-//            try {
-//                DatabaseManager manager = service.connect(databaseName, userName, password);
-//                req.getSession().setAttribute("db_manager", manager);
-//                resp.sendRedirect(resp.encodeRedirectURL("menu"));
-//            } catch (Exception e) {
-//                req.setAttribute("message", e.getMessage());
-//                req.getRequestDispatcher("error.jsp").forward(req, resp);
-//            }
-//        }
-//    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = getAction(req);
+
+        if (action.startsWith("/connect")) {
+            String databaseName = req.getParameter("dbname");
+            String userName = req.getParameter("username");
+            String password = req.getParameter("password");
+
+            try {
+                DatabaseManager manager = service.connect(databaseName, userName, password);
+                req.getSession().setAttribute("db_manager", manager);
+                resp.sendRedirect(resp.encodeRedirectURL("menu"));
+            } catch (Exception e) {
+                req.setAttribute("message", e.getMessage());
+                req.getRequestDispatcher("error.jsp").forward(req, resp);
+            }
+        }
+    }
 
 }
